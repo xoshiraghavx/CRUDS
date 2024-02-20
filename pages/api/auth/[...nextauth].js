9b2f0c3dc9d74ff87b7e31e4c6a5fc464fc6e91b
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2024-02-20 21:43:14 +0530

    Update .gitignore and .env files, and refactor index.js and [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,21 +1,16 @@
-import NextAuth from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
-import { MongoDBAdapter } from "@auth/mongodb-adapter"
-import clientPromise from '@/lib/mongodb'
 
+import clientPromise from '@/lib/mongodb';
+import { MongoDBAdapter } from '@auth/mongodb-adapter';
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
 
 export default NextAuth({
-  
+  adapter: MongoDBAdapter(clientPromise),
   providers: [
-    // OAuth authentication providers...
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET
-    })
-  ], 
-  adapter: MongoDBAdapter(clientPromise),
-  session:{ 
-    strategy: 'jwt'
-
-  }
-})
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+});
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function Home() {
-  const { data: session } = useSession()
+  const { data: session } = useSession();
   if(!session){
     return(
       <div className="bg-blue-900 w-screen h-screen flex items-center">
